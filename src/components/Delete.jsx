dc4ce6ba0252fc4f49fec6baf5d0dda979cbb43f
Pushkardeep <just.pushkardeep@gmail.com>
2024-06-07 11:11:23 +0530

    initial commit

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -40,21 +40,21 @@ function Delete() {
     <>
       <div
         ref={deleleRef}
-        className="absolute w-full h-[100%] px-1.5 py-2 bg-[#18181887] backdrop-blur-sm flex flex-col justify-center items-center gap-5 rounded-b-lg top-0 left-0"
+        className="absolute top-0 left-0 w-full h-[100%] bg-[#181818ad] backdrop-blur-sm flex flex-col justify-center items-center px-3.5 gap-10 z-50"
       >
-        <div className="w-full h-fit flex justify-end px-3 cursor-pointer">
+        <div className="w-full h-fit flex justify-end cursor-pointer">
           <span
             onClick={onUnMount}
-            className="material-symbols-outlined text-white text-[26px]"
+            className="material-symbols-outlined text-white text-[30px]"
           >
             close
           </span>
         </div>
-        <div className="flex flex-col px-2 gap-1 justify-start w-full h-fit">
+        <div className="flex flex-col gap-1 justify-start w-full h-fit">
           <h1 className="text-[13px] text-white">Wanna Delete?</h1>
           <button
             onClick={handleClick}
-            className="text-black w-fit bg-white rounded-lg px-3 py-1.5 text-[12px] font-semibold"
+            className="text-black w-fit bg-white hover:bg-[#c8c8c8] rounded-lg px-4 py-2 text-[12px] font-semibold"
           >
             Delete
           </button>
